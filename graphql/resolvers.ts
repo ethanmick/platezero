f3955a2fd5a4bdb7397e7d2c7dd9ff5d14ee48f1
Ethan Mick <ethan@ethanmick.com>
2022-05-20 15:15:33 -0400

    Got basic recipe page working again

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,7 +1,14 @@
+import { Context } from './context'
+
 export const resolvers = {
   Query: {
-    recipes: (_parent, _args, ctx) => {
-      return ctx.prisma.recipe.findMany()
-    }
-  }
+    recipes: (_parent: never, _args: any, ctx: Context) => {
+      console.log('GET RECIPES')
+      return ctx.prisma.recipe.findMany({
+        where: {
+          userId: ctx.user.id,
+        },
+      })
+    },
+  },
 }
