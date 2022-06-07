894d871bd8fbf7d65d6bf67226c54bd153787aca
Ethan Mick <ethan@ethanmick.com>
2022-06-07 17:10:30 -0400

    Change normalize to be fractions
    This makes it more readable, which is what we want for now.

--- lib/parse/normalize.test.ts
+++ lib/parse/normalize.test.ts
@@ -2,8 +2,8 @@ import { normalize } from './normalize'
 
 test.each([
   ['simple', 'simple'],
-  ['½ cup milk', '1/2 cup milk'],
-  ['&frac14; teaspoon salt', '1/4 teaspoon salt'],
+  ['½ cup milk', '½ cup milk'],
+  ['&frac14; teaspoon salt', '¼ teaspoon salt'],
 ])('normalize(%s)', (s: string, expected: string) => {
   expect(normalize(s)).toEqual(expected)
 })
